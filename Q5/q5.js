01b1fa25a85643ed6811f91fad13fbea329fb55f
Kasper 369 <mohsinakrambukhari@gmail.com>
2023-02-14 16:36:59 +0500

    don q6

---
+++ Q5/q5.js
@@ -0,0 +1,4 @@
+// 5.Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.Then compose your message and store it in a new variable called message.Print your message.
+var famous_person = 'Sir Albert Enistein';
+var message = "\"Never memorize something that you can look up.\" by ".concat(famous_person, " ");
+console.log(message);
