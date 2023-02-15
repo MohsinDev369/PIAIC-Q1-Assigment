2e7130016f2010549a90c5385280305bfb9a0339
Kasper 369 <mohsinakrambukhari@gmail.com>
2023-02-15 21:17:20 +0500

    don till q19

--- Q18/q18.js
+++ Q18/q18.js
@@ -1,31 +1,30 @@
 // 18. Seeing the World: Think of at least five places in the world you’d like to visit.
-
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 //• Store the locations in a array. Make sure the array is not in alphabetical order.
-let locations = ['France', 'Silicon valley', 'London', 'United Kingdom', 'Dubai'];
-
+var locations = ['France', 'Silicon valley', 'London', 'United Kingdom', 'Dubai'];
 //• Print your array in its original order.
-console.log(locations); 
-
+console.log(locations);
 //• Print your array in alphabetical order without modifying the actual list.
-console.log([...locations].sort()); 
-
+console.log(__spreadArray([], locations, true).sort());
 //• Show that your array is still in its original order by printing it.
 console.log(locations);
-
 //• Print your array in reverse alphabetical order without changing the order of the original list.
-console.log([...locations].sort().reverse());
-
+console.log(__spreadArray([], locations, true).sort().reverse());
 // • Show that your array is still in its original order by printing it again.
 console.log(locations);
-
 //• Reverse the order of your list. Print the array to show that its order has changed.
 console.log(locations.reverse());
-
 //• Reverse the order of your list again. Print the list to show it’s back to its original order.
 console.log(locations.reverse());
-
 //• Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
 console.log(locations.sort());
-
 //• Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
 console.log(locations.reverse());
