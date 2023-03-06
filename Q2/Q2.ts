63b6a0854fb6c99f7a063f316db9bd9c497fee52
Kasper 369 <mohsinakrambukhari@gmail.com>
2023-03-06 22:52:24 +0500

    adding type

--- Q2/Q2.ts
+++ Q2/Q2.ts
@@ -1,4 +1,4 @@
 // 2.Personal Message: Store a person’s name in a variable, and print a message to that person.Your message should be simple, such as, “Hello Eric, would you like to learn some Python today ?”
 
-let namee = 'Mohsin'
+let namee:string = 'Mohsin'
 console.log("Hello " + namee + ", would you like to learn some Javascript today?")
