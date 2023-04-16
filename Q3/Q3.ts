6831b9662cf83d0d541dd4093f2accbf3bcfab28
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-04-16 21:06:46 +0500

    changes to types

--- Q3/Q3.ts
+++ Q3/Q3.ts
@@ -1,6 +1,6 @@
 // 3.Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
 
-let personName = 'mohsin'
+let personName:string = 'mohsin'
 
 // printing the namer in lowercase
 console.log(personName.toLowerCase()); 
